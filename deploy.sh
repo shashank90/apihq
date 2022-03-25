bf8e844eb3c938794c2439fe311f402f7bd940ed
Shashank Gudipati <sgshanks@gmail.com>
2022-03-25 12:52:52 +0530

    Add on-premise deployment support

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-# Assuming Ubuntu OS
+# For Ubuntu OS
 
+echo "Untar deploy folder"
+tar -xvf deploy_artifacts.tar.gz
+
+echo "Install docker"
 # Install docker
 sudo snap install docker
 # Make docker run for non-sudo users
 sudo groupadd docker
 sudo gpasswd -a $USER docker
+sudo touch /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
-newgrp docker
 
 
+echo "Install docker-compose"
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 
+echo "Install certbot"
+sudo snap install --classic certbot 
+
+echo "Create dirs"
 # Create folders
 cd ~
 mkdir -p apihome/docker
@@ -36,14 +45,33 @@ mkdir -p volumes/app/logs
 mkdir -p volumes/db/pgdata 
 
 # Docker volume for managing certs
-mdkir -p volumes/web/certbot/www/
+mkdir -p volumes/web/nginx/conf
+mkdir -p volumes/web/certbot/www/
 mkdir -p volumes/web/certbot/certificates
 
 
 # Pull images
-docker pull sgshanks/apihome:ui
-docker pull sgshanks/apihome:backend
-docker pull sgshanks/apihome:db
+echo "Docker login"
+cd ~/apihome/docker
+cp ~/deploy_artifacts/docker_login_password.txt .
+cat docker_login_password.txt | sudo docker login --username sgshanks --password-stdin
 
+echo "Pulling docker images"
+sudo docker pull sgshanks/apihome:ui
+sudo docker pull sgshanks/apihome:backend
+sudo docker pull sgshanks/apihome:db
 
-cat ~/my_password.txt | docker login --username sgshanks --password-stdin
+echo "Copy start and stop scripts to docker dir"
+cp ~/deploy_artifacts/start.sh ~/apihome/docker
+cp ~/deploy_artifacts/stop.sh ~/apihome/docker
+
+echo "Copy docker compose script"
+cp ~/deploy_artifacts/docker-compose.yaml ~/apihome/docker
+
+echo "Copy nginx conf"
+cp ~/deploy_artifacts/apihome_on_prem_80.nginx ~/apihome/docker/volumes/web/nginx/conf/apihome_on_prem_80.nginx
+
+echo "Make docker non-sudo"
+newgrp docker
+
+exit
--- docker/prod/certs/on-prem/get_cert_cmd_on_prem.sh
+++ docker/prod/certs/on-prem/get_cert_cmd_on_prem.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+
 # from ~/apihome/docker
 docker-compose -f docker-compose-cert-renew.yaml run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d www.test.apihome.io
